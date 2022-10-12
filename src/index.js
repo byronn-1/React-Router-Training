05c9e12473687f0ad82ba214ac48e772b89ec154
Byron Lee <byronn.lee@gmail.com>
2022-10-12 14:27:18 +0100

    added routing to app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 
+
+// wrap the app in browser router component 
+// don't for get => npm install react-router-dom
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
